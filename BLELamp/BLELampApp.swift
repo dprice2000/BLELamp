bba027d55247015d4a4eb53270e858dccdb97d48
dprice2000 <59681432+dprice2000@users.noreply.github.com>
2025-04-26 22:34:45 -0700

    Initial Commit

---
+++ BLELamp/BLELampApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLELampApp.swift
+//  BLELamp
+//
+//  Created by David Price on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BLELampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLELampTests/BLELampTests.swift
@@ -0,0 +1,17 @@
+//
+//  BLELampTests.swift
+//  BLELampTests
+//
+//  Created by David Price on 4/26/25.
+//
+
+import Testing
+@testable import BLELamp
+
+struct BLELampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
